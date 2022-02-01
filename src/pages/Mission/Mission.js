6b35472e52514dbc393482bd88aeff733be460db
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-02-01 13:51:47 +0100

    Fetch Missions and render it

--- src/pages/Mission/Mission.js
+++ src/pages/Mission/Mission.js
@@ -1,10 +1,14 @@
-const Mission = () => (
-  <tr>
-    <th scope="row">1</th>
-    <td>Mark</td>
-    <td>Otto</td>
-    <td>@mdo</td>
-  </tr>
-);
+/* eslint-disable */
+const Mission = (mission) => {
+  const { id, name, description } = mission;
+  return (
+    <tr>
+      <th scope="row">{name}</th>
+      <td>{description}</td>
+      <td className="align-middle"><span className="badge bg-secondary">Not A MEMBER</span></td>
+      <td className="align-middle"><button type="button" className="btn btn-outline-secondary">Join Mission</button></td>
+    </tr>
+  );
+};
 
 export default Mission;
