faa8cd991f102d99ed2bea233f2303f83361c448
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-02-02 18:41:11 +0100

    Fetch Rockets Data from API and render it

--- src/pages/Rocket/Rocket.js
+++ src/pages/Rocket/Rocket.js
@@ -1,16 +1,22 @@
-const Rocket = () => {
-  const logo = 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80';
+/* eslint-disable */
+const Rocket = (rocket) => {
+  const {
+    id,
+    name,
+    image,
+    description,
+  } = rocket;
   return (
     <div className="card mb-3">
       <div className="row">
         <div className="col-md-3">
-          <img src={logo} alt="Logo" style={{ maxWidth: '280px', height: '100%' }} />
+          <img src={image} alt="Logo" style={{ maxWidth: '100%', height: '100%' }} />
         </div>
         <div className="col-md-9">
           <div className="card-body">
-            <h5 className="card-title ">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button type="button" className="btn btn-primary">Primary</button>
+            <h5 className="card-title ">{name}</h5>
+            <p className="card-text">{description}</p>
+            <button type="button" className="btn btn-primary">Reserve Rocket</button>
           </div>
         </div>
       </div>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,8 +4,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import missionReducer from './Missions/Missions';
+import rockectsReducer from './Rockets/Rockets';
 
-const reducer = combineReducers({ missionReducer });
+const reducer = combineReducers({ missionReducer, rockectsReducer });
 
 const store = createStore(
   reducer,
