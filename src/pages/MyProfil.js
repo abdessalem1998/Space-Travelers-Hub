15ca27b07662ef4e794fdb1aa55ede490057412e
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-02-03 09:15:31 +0100

    Design the Profile page using bootstrap.

--- src/pages/MyProfil.js
+++ src/pages/MyProfil.js
@@ -1,5 +1,43 @@
 const MyProfile = () => (
-  <div>profile</div>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm">
+        <h2>My Missions</h2>
+        <table className="table table-bordered">
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="col-sm">
+        <h2>My Rockets</h2>
+        <table className="table table-bordered">
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 );
 
 export default MyProfile;
